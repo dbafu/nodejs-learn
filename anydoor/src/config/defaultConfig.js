dd028e9cfe2e12869a7972af31010edc835619a9
dbafu <damon.fu@qq.com>
2018-09-25 21:24:48 +0800

    re-init

--- anydoor/src/config/defaultConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  root: process.cwd(), 
-  host: '127.0.0.1',
-  port: 3000
-}
--- gulp-demo/gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-
-gulp.task('default', () => {
-  // console.log('default task');
-  gulp.src(['src/**/*'])
-    .pipe(gulp.dest('build'))
-})
